4014e07f0c5de4b05ef61134633b732674f23734
JenniferFadare <70460125+JenniferFadare@users.noreply.github.com>
2021-02-02 15:29:42 -0600

    module 4.3.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event){
     event.preventDefault();
@@ -27,13 +28,17 @@ var createTaskEl = function (taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
     taskInfoEl.innerHTML ="<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>"
      + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
+
+    taskIdCounter++;
 }
 
 //holds the code that creates a new task HTML element.
